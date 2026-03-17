952642ffdc3a052b6fc74c4fc99cefb25ecd667b
alexnguyen <tuan.nguyen2@finhay.com.vn>
2026-03-17 13:33:20 +0700

    Update SquaresOfSortedArray.java

--- src/com/dsa_in_90_days/twopointers/SquaresOfSortedArray.java
+++ src/com/dsa_in_90_days/twopointers/SquaresOfSortedArray.java
@@ -5,13 +5,66 @@ import java.util.Arrays;
 //https://leetcode.com/problems/squares-of-a-sorted-array
 public class SquaresOfSortedArray {
 
-    public int[] sortedSquares(int[] nums) {
-        int[] rs = new int[nums.length];
+    public static int[] sortedSquares(int[] nums) {
         for (int i = 0; i < nums.length; i++) {
-            rs[i] = nums[i] * nums[i];
+            nums[i] = nums[i] * nums[i];
         }
-        Arrays.sort(rs);
+        int[] rs = sort(nums);
         return rs;
+    }
+
+    private static int[] sort(int[] nums) {
+        int minIndex = getMinValueIndex(nums);
+        int i = minIndex;
+        int j = minIndex + 1;
+        int indexCounter = 0;
+        int[] rs = new int[nums.length];
+        while (i >= 0 || j < nums.length) {
+            if (j == nums.length) {
+                rs[indexCounter] = nums[i];
+                i--;
+                indexCounter++;
+                continue;
+            }
+            if (i < 0) {
+                rs[indexCounter] = nums[j];
+                j++;
+                indexCounter++;
+                continue;
+            }
+
+            int valI = nums[i];
+            int valJ = nums[j];
+            if (valI > valJ) {
+                rs[indexCounter] = valJ;
+                j++;
+            } else {
+                rs[indexCounter] = valI;
+                i--;
+            }
+            indexCounter++;
+        }
+        return rs;
+    }
+
+    private static int getMinValueIndex(int[] nums) {
+        int minIndex = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i == 0) {
+                continue;
+            }
+            var minValue = nums[minIndex];
+            if (nums[i] < minValue) {
+                minIndex = i;
+            }
+        }
+        return minIndex;
+    }
+
+    public static void main(String[] args) {
+        int[] array = new int[] {10};
+        int[] squareds = sortedSquares(array);
+        System.out.println(squareds);
     }
 
 }
