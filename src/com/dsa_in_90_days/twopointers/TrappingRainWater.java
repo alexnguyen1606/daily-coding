618ddcf6c1f577914eee82e8fcd5438b42f06f61
alexnguyen1606 <125424220+tuannguyen2finhay@users.noreply.github.com>
2026-03-24 00:14:15 +0700

    Create TrappingRainWater.java

---
+++ src/com/dsa_in_90_days/twopointers/TrappingRainWater.java
@@ -0,0 +1,58 @@
+package com.dsa_in_90_days.twopointers;
+
+public class TrappingRainWater {
+
+    public static int trap(int[] height) {
+        int totalTrap = 0;
+        int left = 0;
+        int right = 0;
+        while (right < height.length) {
+            int delta = right - left;
+            if (delta <= 1) {
+                right++;
+                continue;
+            }
+            if (height[right] == 0) {
+                right++;
+                continue;
+            }
+            if (height[left] == 0) {
+                left++;
+                continue;
+            }
+            int maxTrap = 0;
+            while (right < height.length) {
+                int trap = trap(left, right, height);
+                
+                maxTrap = Math.max(trap, maxTrap);
+
+                right++;
+            }
+            totalTrap += maxTrap;
+            left = right;
+
+        }
+        return totalTrap;
+    }
+
+    private static int trap(int i, int j, int[] height) {
+        int width = Math.abs(i - j) - 1;
+        int maxTrap = Math.min(height[i], height[j]) * width;
+        int k = i + 1;
+        while (k < j && k < height.length) {
+            if (height[k] > height[i] || height[k] > height[j]) {
+                maxTrap -= Math.min(height[i], height[j]);
+            } else {
+                maxTrap -= height[k];
+            }
+            k++;
+        }
+        return maxTrap;
+    }
+
+    public static void main(String[] args) {
+        int[] array = new int[]{0,1,0,2,1,0,1,3,2,1,2,1};
+        System.out.println(trap( array));
+    }
+
+}
